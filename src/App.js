548eb77858870c869ee003219308667d1ec98b7d
--path <goodhopeordu@yahoo.com>
2019-06-28 11:31:19 +0100

    iframe

--- src/App.js
+++ src/App.js
@@ -6,15 +6,46 @@ import './assets/css/App.css';
 import Home from './pages/home';
 import Episodes from './pages/episodes';
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: []
+    }
+
+  }
+
+  onLoadMovieList(e) {
+    var p = e.target.getElementsByClassName("data_list")
+
+    var series = []
+
+    if (p.length > 0) {
+        p = p.item(0).getElementsByClassName("data")
+
+        for (let i = 0; i < p.length; i++) {
+            const a = p.item(i).getElementsByTagName("a");
+
+            if (a.length > 0) {
+                var name = a.item(0).innerHTML
+                series.push(name)
+            }
+        }
+        this.setState({list: series})
+    }
+  }
+
   render() {
     return (
       <div style={{position: 'relative', backgroundColor: "rgb(51,51,51)", minHeight: '100vh', overflowX: "hidden"}}>
         <BrowserRouter>
           <div>
-            <Route exact path="/" component={Home} />
-            <EpisodesRoute path="/:name"/>
-            <EpisodesRoute path="/:name/:season"/>
+            <Route exact path="/" render={() => <Home list={this.state.list}/>} />
+            <EpisodesRoute path="/:name" list={this.state.list}/>
+            <EpisodesRoute path="/:name/:season" list={this.state.list}/>
           </div>
         </BrowserRouter>
       </div>
@@ -22,7 +53,7 @@ class App extends Component {
   }  
 }
 
-function EpisodesRoute({path}) {
+function EpisodesRoute({path, list}) {
   return (
     <Route
       exact
@@ -39,7 +70,7 @@ function EpisodesRoute({path}) {
         if (name) {
           name = name.replace(/-/g, " ")
         }
-        return <Episodes name={name} season={season}/>
+        return <Episodes name={name} season={season}  list={list}/>
       }}
     />
   )
